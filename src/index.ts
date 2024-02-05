381ebd60abb231c8a9e80c57077054adcc7c2e8a
“Ermias <gearmias@gmail.com>
2024-02-05 13:39:48 +0300

    added logging lob with winston and morgan

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // src/index.js
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import { LoggerStream } from "./logger/logger";
 
 dotenv.config();
 
@@ -9,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(morgan('combined', { stream: new LoggerStream() }));
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({msg: "Express and TypeScript starter!"});
 });
---
+++ src/logger/logger.ts
@@ -0,0 +1,37 @@
+import winston from "winston";
+
+const options = {
+  file: {
+    level: "info",
+    filename: `../../logs/logs.log`,
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+  },
+  console: {
+    level: "debug",
+    handleExceptions: true,
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+  },
+};
+
+export const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+export class LoggerStream {
+    write(message: string) {
+        logger.info(message.substring(0, message.lastIndexOf('\n')));
+    }
+}

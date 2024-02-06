a2448cd72f05ca21d5e00a21d9b63765af370ac4
“Ermias <gearmias@gmail.com>
2024-02-06 10:46:52 +0300

    added update endpoint

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,19 +1,18 @@
-import { Request, Response, NextFunction} from "express";
-import Joi from "joi";
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 
 export class TypeValidator<T> {
   validate = (schema: Joi.ObjectSchema) => {
     return async (req: Request, res: Response, next: NextFunction) => {
       try {
         const val = await schema.validateAsync(req.body);
-        console.log("Current val", val);
         // req.body = val as AddFormToJobType;
         req.body = val as T;
         next();
       } catch (err) {
         const messages = (err as Joi.ValidationError)?.details
           .map((i) => i.message)
-          .join(",");
+          .join(',');
 
         res.status(422).json({
           success: false,
@@ -23,21 +22,3 @@ export class TypeValidator<T> {
     };
   };
 }
-
-// export const jobUpdateSchema = Joi.object().keys({
-//   title: Joi.string().trim(),
-//   email: Joi.string().email(),
-//   closingDate: Joi.date(),
-//   description: Joi.string().trim(),
-//   salary: Joi.number(),
-//   companyId: Joi.string().trim(),
-//   currency: Joi.string().valid("USD", "ETB"),
-//   benefits: Joi.string().trim(),
-//   experienceNeeded: Joi.number().min(0),
-//   region: Joi.string().trim(),
-//   city: Joi.string().trim(),
-//   internalCode: Joi.string().trim(),
-//   quantity: Joi.number().min(0),
-//   status: Joi.string(),
-
-// });
